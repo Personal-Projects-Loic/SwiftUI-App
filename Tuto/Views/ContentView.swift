f94b0eb3ee603edb7a9eb62e1f5827e7bf2d85d6
loic-rouzaud <loic.rouzaud@epitech.eu>
2023-12-28 15:30:26 +0100

    [ADD] - doc + list of all differents parks using Identifable protocol using unique ID💻

--- Tuto/Views/ContentView.swift
+++ Tuto/Views/ContentView.swift
@@ -2,36 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -150)
-                .padding(.bottom, -130)
-
-            VStack(alignment: .leading) {
-                Text("Perros-Guirec")
-                    .font(.title)
-                    .fontWeight(.bold)
-                HStack(alignment: .center) {
-                    Text("City in France")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("22540")
-                        .font(.subheadline)
-                }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-                
-                Divider()
-                    Text("About Turtle Rock")
-                        .font(.title2)
-                    Text("Descriptive text goes here.")
-            }
-            .padding()
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
--- Tuto/Views/LandmarkList.swift
+++ Tuto/Views/LandmarkList.swift
@@ -1,16 +1,24 @@
-//
-//  LandmarkList.swift
-//  Tuto
-//
-//  Created by Loïc Rouzaud on 28/12/2023.
-//
+
+// NavigationSplitView : Used as navigation in React.
+// With this embeded fonction, i can create a sidebar, a view, a new content, and details.
+
+
 
 import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks, id: \.id) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationSplitView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
         }
     }
 }
